1dffad646c60153ea16803cc0e815ae0815beef5
mx2240 <lovemicky10@gmail.com>
2025-12-17 13:26:05 +0000

    u

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ mongoose.connect(mongoURI)
 
 
 // --- Start server ---
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 
